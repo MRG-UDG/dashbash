1280ad00e32cf8dbc60871d709ef6935c60139cc
Marko Röper-Grewe <marko.roeper-grewe@udg.de>
2024-08-27 20:38:19 +0200

    [TASK] - Database query extended and output of CTypes based on language versions and subdivided for plugins by plugin type

--- Classes/Widgets/CTypesWidget.php
+++ Classes/Widgets/CTypesWidget.php
@@ -1,8 +1,9 @@
 <?php
 namespace MRG\Dashbash\Widgets;
 
-use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
 use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
 use TYPO3\CMS\Fluid\View\StandaloneView;
@@ -11,33 +12,119 @@ class CTypesWidget implements WidgetInterface
 {
     private WidgetConfigurationInterface $configuration;
     private StandaloneView $view;
+    private SiteFinder $siteFinder;
 
     public function __construct(
         WidgetConfigurationInterface $configuration,
-        StandaloneView $view
+        StandaloneView $view,
+        SiteFinder $siteFinder
     ) {
         $this->configuration = $configuration;
         $this->view = $view;
+        $this->siteFinder = $siteFinder;
     }
 
     public function renderWidgetContent(): string
     {
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
-        $result = $queryBuilder
-            ->select('CType')
+        $availableCTypes = $this->getAvailableCTypes();
+        $availableListTypes = $this->getListTypes();
+        $usedLanguages = $this->getUsedLanguages();
+
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('tt_content');
+        $results = $queryBuilder
+            ->select('*')
             ->addSelectLiteral('COUNT(*) AS count')
             ->from('tt_content')
-            ->groupBy('CType')
+            ->groupBy('sys_language_uid', 'CType', 'list_type')
             ->executeQuery()
             ->fetchAllAssociative();
 
+        $ctypes = [];
+        $langaugeUids = [];
+        foreach ($results as $result) {
+            $langaugeUids[] = $result['sys_language_uid'];
+            if (!isset($ctypes[$result['CType']])) {
+                $ctypes[$result['CType']] = [
+                    $result['sys_language_uid'] => $result['count']
+                ];
+            } else {
+                $ctypes[$result['CType']][$result['sys_language_uid']] = $result['count'];
+            }
+        }
+        $langaugeUids = array_unique($langaugeUids);
+
         $this->view->setTemplatePathAndFilename('EXT:dashbash/Resources/Private/Templates/Widget/CTypesWidget.html');
         $this->view->assignMultiple([
+            'title' => 'CTypes Overview',
             'configuration' => $this->configuration,
-            'ctypes' => $result,
+            'ctypes' => $ctypes,
+            'langaugeUids' => $langaugeUids,
+            'availableCTypes' => $availableCTypes,
+            'usedLanguages' => $usedLanguages,
+            'availableListTypes' => $availableListTypes,
         ]);
 
         return $this->view->render();
+    }
+
+    private function getAvailableCTypes(): array
+    {
+        $tcaCtypes = $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items'];
+        $availableCTypes = [];
+        foreach ($tcaCtypes as $ctype) {
+            if (is_array($ctype) && isset($ctype[0], $ctype[1])) {
+                $availableCTypes[$ctype[1]] = [
+                    'name' => $ctype[0],
+                    'ctype' => $ctype[1],
+                    'listTypes' => ($ctype[1] === 'list') ? $this->getListTypes() : []
+                ];
+            }
+        }
+        return $availableCTypes;
+    }
+
+    private function getListTypes(): array
+    {
+        $listTypes = [];
+        $tcaListTypes = $GLOBALS['TCA']['tt_content']['columns']['list_type']['config']['items'];
+        foreach ($tcaListTypes as $listType) {
+            if (is_array($listType) && isset($listType[0], $listType[1]) && $listType[0] != '') {
+                $listTypes[$listType[1]] = $listType[0];
+            }
+        }
+        return $listTypes;
+    }
+
+    private function getUsedLanguages(): array
+    {
+        $usedLanguages = [];
+        $sites = $this->siteFinder->getAllSites();
+
+        foreach ($sites as $site) {
+            foreach ($site->getAllLanguages() as $language) {
+                $languageId = $language->getLanguageId();
+                if (!isset($usedLanguages[$languageId])) {
+                    $usedLanguages[$languageId] = [
+                        'title' => $language->getTitle(),
+                        'twoLetterIsoCode' => $language->getTwoLetterIsoCode(),
+                        'hreflang' => $language->getHreflang(),
+                        'siteName' => $site->getIdentifier(),
+                        'flagIdentifier' => $language->getFlagIdentifier(),
+                    ];
+                } else {
+                    // If the language is used in multiple sites, add the site name to the list
+                    if (!str_contains($usedLanguages[$languageId]['siteName'], $site->getIdentifier())) {
+                        $usedLanguages[$languageId]['siteName'] .= ', ' . $site->getIdentifier();
+                    }
+                }
+            }
+        }
+
+        // Sort languages by their ID
+        ksort($usedLanguages);
+
+        return $usedLanguages;
     }
 
     public function getOptions(): array
--- ext_emconf.php
+++ ext_emconf.php
@@ -6,12 +6,23 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'be',
     'author' => 'Marko Röper-Grewe',
     'author_email' => 'marko.roeper-grewe@udg.de',
-    'state' => 'stable',
-    'version' => '1.0.0',
+    'author_company' => 'PIA / UDG',
+    'state' => 'beta',
+    'clearCacheOnLoad' => 1,
+    'version' => '0.1.0',
+    'iconIdentifier' => 'ext-dashbash-icon',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
             'dashboard' => '11.5.0-12.4.99',
         ],
+        'conflicts' => [],
+        'suggests' => [],
     ],
+    'autoload' => [
+        'psr-4' => [
+            'MRG\\Dashbash\\' => 'Classes/'
+        ]
+    ],
+    'loadInBackend' => true,
 ];
