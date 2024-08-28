dc6344a34a3cfaa9f35b1897aa60bd457d139125
Marko Röper-Grewe <marko.roeper-grewe@udg.de>
2024-08-28 03:49:10 +0200

    [TASK] more Icons added

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,23 @@
 defined('TYPO3') or die();
 
 (static function() {
+
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
         '@import "EXT:dashbash/Configuration/TypoScript/setup.typoscript"'
     );
+
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Imaging\IconRegistry::class
+    );
+    $iconRegistry->registerIcon(
+        'ext-dashbash-icon',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:dashbash/Resources/Public/Icons/dashbash-icon.svg']
+    );
+    $iconRegistry->registerIcon(
+        'content-widget-dashbash',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:dashbash/Resources/Public/Icons/content-widget-dashbash.svg']
+    );
+
 })();
